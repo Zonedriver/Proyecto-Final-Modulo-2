dabcc5ad8e4e866e5f1ff4c225115f0f9f746dbc
Zonedriver <96269794+Zonedriver@users.noreply.github.com>
2022-02-18 18:08:47 -0500

    Add files via upload

---
+++ pj-final/src/components/tileData.js
@@ -0,0 +1,39 @@
+const tileData = [
+    {
+    img: 'https://ramenparados.com/wp-content/uploads/2009/11/seto-no-hanayome.jpg', 
+    sino: 'https://mybrideisamermaid.fandom.com/wiki/Seto_no_Hanayome',
+    title: 'Seto no Hanayome',
+    },
+    {
+    img: 'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2013/04/218604-fallece-autor-zero-no-tsukaima.jpg?itok=asQGL9Pw',
+    title: 'Zero no Tsukaima',
+    featured: true,
+    },
+    {
+      img: 'https://m.media-amazon.com/images/M/MV5BMmVkNTMwNjMtMjhjMi00ZWQwLWIzYWEtZmI3ZDk1MTk4MjdjXkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_FMjpg_UX1000_.jpg',
+      title: 'Shakugan no Shana',
+    },
+    {
+      img: 'https://ataglanceanimehome.files.wordpress.com/2021/10/guiltycrown.jpg',
+      title: 'Guilty Crown',
+    },
+    {
+      img: 'https://m.media-amazon.com/images/M/MV5BMjUxMzE4ZDctODNjMS00MzIwLThjNDktODkwYjc5YWU0MDc0XkEyXkFqcGdeQXVyNjc3OTE4Nzk@._V1_FMjpg_UX1000_.jpg',
+      title: 'Steins; Gate',
+    },
+    {
+      img: 'https://www.otaquest.com/wp-content/uploads/2020/11/Sankarea.jpg',
+      title: 'Sankarea',
+      featured: true,
+    },
+    {
+      img: 'https://i.pinimg.com/originals/81/b9/af/81b9af426e10c511d3b63dd300675266.jpg',
+      title: 'Kore wa zombie desuka?',
+    },
+    {
+      img: 'https://www.thereportertimes.com/wp-content/uploads/2018/07/air-gear-season-2.jpg',
+      title: 'Air Gear',
+    },
+  ];
+  
+  export default tileData;
